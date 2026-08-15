d2ee4c272d7a7b5f45be727db18d34c68d0a5d1b
rayangd <rayanhd168@gmail.com>
2026-08-15 18:54:27 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,224 @@
+<?php
+$nom = "aloura";
+$ages = 17 ;
+echo ("bonjours Mr $nom vos avez $ages ans");
+?>*{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
+body {
+    display: flex;
+    width: 100%;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    overflow: hidden;
+    background-color: #17132f;
+
+    background-image:
+        linear-gradient(45deg, rgba(104, 47, 91, 0.30) 1px, transparent 1px),
+        linear-gradient(-45deg, rgba(104, 47, 91, 0.30) 1px, transparent 1px);
+
+}
+html, body {
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    overflow: hidden;
+}
+
+#formulaire{
+  
+    justify-content: center;
+    align-items: center;
+   background-color: white;
+   border-radius:10px;
+   width: 420px;
+   height: 490px;
+   padding-left: 20px;
+}
+.inpu{
+    width: 340px;
+}
+#formulaire input{
+    width: 100%;
+    margin: 10px 0;
+    padding: 10px;
+    box-sizing: border-box;
+    border: 1px solid rgb(177, 177, 138);
+
+}
+   <p class="dj">Déjà inscrit ? <a href="#" onclick="toggleInscription(); toggleConnexion(); return false;">Se connecter</a></p>
+.accepte{
+    position: absolute;
+    top: 418px;
+    right: 460px;
+    color: black;
+}
+        <button class="insc" onclick="toggleInscription();">Créer un compte gratuit</button>
+        <button class="deja" onclick="toggleConnexion();">J'ai déjà un compte</button>
+.envoie{
+    position: absolute;
+   background-color: blueviolet;
+    padding: 10px;
+    width: 350px;
+    color: white;
+    top: 450px;
+    border-radius: 15px;
+}
+h5{
+    padding: 15px;
+    color: blueviolet;
+}
+#formulaire .checkbox{
+    position: absolute;
+    top: 410px;
+    right: 620px;
+    width: 200px;
+
+
+}
+.dj{
+    position: absolute;
+    top: 510px;
+    left: 550px ;
+    color: black;
+    
+}
+td{
+    color:rgb(177, 177, 138) ;
+}
+a{
+    color: black;
+}
+#formulair{
+  display: none;
+    justify-content: center;
+    align-items: center;
+   background-color: white;
+   border-radius:10px;
+   width: 470px;
+   height: 490px;
+   padding-left: 20px;
+}
+#formulair input{
+    width: 95%;
+    margin: 10px 0;
+    padding: 10px;
+    box-sizing: border-box;
+    border: 1px solid rgb(177, 177, 138);
+
+}
+.envoi{
+    position: absolute;
+    top: 400px;
+   background-color: rgb(58, 136, 125);
+    padding: 10px;
+    width: 350px;
+    color: white;
+    border-radius: 15px;
+    right: 480px;
+}
+.accept{
+    position: absolute;
+    top: 350px;
+    right: 720px;
+    color: black;
+
+}
+#formulair .checkbox{
+    position: absolute;
+    top: 340px;
+    right: 740px;
+    width: 200px;
+
+
+}
+.conne{
+    color: rgb(58, 136, 125);
+}
+label{
+    color: rgb(177, 177, 138);
+}
+img{
+    position: absolute;
+    top: 4px;
+    height: 50px;
+    width: 50px;
+    border-radius: 1px;
+    left: 40px;
+    border-radius: 10px;
+}
+h3{
+    position: absolute;
+    top: 10px;
+    left: 100px;
+}
+.jaune{
+     position: absolute;
+    top: 10px;
+    left: 160px;
+    color: orange;
+}
+.info{
+   position: absolute;
+   top: 30px;
+   left: 100px;
+   font-size: 10px;
+}
+nav{
+    position: absolute;
+    top: 10px;
+    left: 250px;
+    gap: 15px;
+}
+nav a{
+    text-decoration: none;
+    gap: 15px;
+    padding: 10px;
+    border-radius: 15px;
+}
+.culture{
+    background-color: blueviolet;
+    color: white;
+}
+.musique{
+    background-color: red;
+    color: white;
+}
+.sport{
+    background-color: rgb(35, 147, 147);
+    color: white;
+
+}
+.high-tech{
+    background-color: orange;
+    color: black;
+}
+.nav a{
+    position: absolute;
+    left: 600px;
+    top: 0;
+    
+}
+.connection {
+    position: absolute;
+    top: 5px;
+   right: 110px;
+    text-decoration: none;
+    padding: 10px;
+    border: 1px solid wheat;
+    border-radius:10px ;
+    color: white;
+}
+.inscription{
+     position: absolute;
+    top: 5px;
+     right: 20px;
+    text-decoration: none;
+    background-color: orange;
+    padding: 10px;
+    border-radius: 10px;
+}
+
